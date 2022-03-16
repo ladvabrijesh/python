9e88fe1147cebccfb1c7cadca2ea57cfc4380579
ladvabrijesh <97820043+ladvabrijesh@users.noreply.github.com>
2022-03-16 20:16:47 +0530

    Add files via upload

---
+++ assignment4/practical8_python.py
@@ -0,0 +1,21 @@
+STACK.append('A')
+
+STACK.append('B')
+
+STACK.append('C')
+
+print('INITIAL STACK')
+
+print(STACK)
+
+print('\nELEMENTS POPPED FROM STACK:')
+
+print(STACK.pop())
+
+print(STACK.pop())
+
+print(STACK.pop())
+
+print("\nSTACK AFTER ELEMENTS ARE POPPED:")
+
+print(STACK)
